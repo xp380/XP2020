4cc58125c0a2806e8c244a0e697a210e5c46bcb0
Vincent Kouoï <vincentkouoi@gmail.com>
2020-12-06 18:32:15 +0100

    in progress

--- FRONT/src/components/AboutMe.js
+++ FRONT/src/components/AboutMe.js
@@ -1,5 +1,24 @@
 import React, {useState} from 'react'
+import { Tabs } from 'antd';
+import avatar from './avatar-gratuit.png'
 
+const { TabPane } = Tabs;
+
+function callback(key) {
+    console.log(key);
+  }
 export default function AboutMe() {
-    return <div>C'est moi</div>
+    return <><div style={{textAlign: 'center'}}>C'est moi</div> <img src={avatar} style={{width:100 }} alt="logo" />
+    <Tabs defaultActiveKey="1" onChange={callback}>
+    <TabPane tab="Qui suis-je ?" key="1">
+      Content of Tab Pane 1
+    </TabPane>
+    <TabPane tab="Mon Parcours" key="2">
+      Content of Tab Pane 2
+    </TabPane>
+    <TabPane tab="Mon CV" key="3">
+      Content of Tab Pane 3
+    </TabPane>
+  </Tabs>
+  </>
 }
