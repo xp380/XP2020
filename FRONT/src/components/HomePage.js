920ad242bea6cf57f796b380e71196c41d06e952
Vincent Kouoï <vincentkouoi@gmail.com>
2020-12-15 15:19:32 +0100

    weather in progress

--- FRONT/src/components/HomePage.js
+++ FRONT/src/components/HomePage.js
@@ -1,6 +1,8 @@
 import React, { Suspense } from 'react'
 import { useTranslation } from "react-i18next";
 
+import Weather from './Weather/Weather'
+import Pollution from './Pollution/Pollution'
 
 function Welcome() {
   const { t } = useTranslation(['traduction']);
@@ -12,8 +14,8 @@ function Welcome() {
             __html: t("title", { name: "Vincent" }),
           }}
         />
-        {/* <button onClick={changeToEnglish}><img src={english} style={{width:100, height:100 }} alt="logo" /></button>
-        <button onClick={changeToFrench}><img src={france} style={{width:100, height: 100 }} alt="logo" /></button> */}
+        <Weather />
+        <Pollution />
       </header>
     </div>
   );
---
+++ FRONT/src/components/Pollution/Pollution.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Pollution() {
+
+    useEffect(() => {
+        fetch('https://api.waqi.info/feed/paris/?token=dc9894cd6eeb6308a58fc373453687f6a62f761a ')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }, [])
+    return (
+        <>
+
+        </>
+    )
+}
+// dc9894cd6eeb6308a58fc373453687f6a62f761a
+// https://api.waqi.info/feed/beijing/?token=dc9894cd6eeb6308a58fc373453687f6a62f761a 
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
