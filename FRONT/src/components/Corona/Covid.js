5e68ab0d049d40543c5225bf83d69b3bb510c0e1
Vincent Kouoï <vincentkouoi@gmail.com>
2020-12-30 17:06:10 +0100

    chat

--- FRONT/src/components/Corona/Covid.js
+++ FRONT/src/components/Corona/Covid.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react'
 
 export default function Covid() {
 
-    // useEffect(() => {
-    //     fetch('https://api.waqi.info/feed/paris/?token=dc9894cd6eeb6308a58fc373453687f6a62f761a ')
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data)
-    //         })
-    // }, [])
+    useEffect(() => {
+        fetch("https://coronavirusapi-france.now.sh/AllDataByDepartement?Departement=Paris")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }, [])
     return (
         <>
 
