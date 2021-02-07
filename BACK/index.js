226c306e14a97b24e70afb5fac627d333e0d7510
Vincent Kouoï <vincentkouoi@gmail.com>
2021-02-07 15:18:18 +0100

    new authentication in back

--- BACK/index.js
+++ BACK/index.js
@@ -1,25 +1,53 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const bodyParser = require("body-parser");
 const cors = require('cors');
-
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
-
-const router = require('./router');
+const dbConfig = require("./app/config/db.config");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+const router = require('./router');
+
+
 app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(router);
 
 app.use('/login', (req, res) => {
     res.send({
-      token: 'test123'
+        token: 'test123'
     });
-  });
+});
 
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+// partie sur les sockets
 io.on('connect', (socket) => {
     socket.on('join', ({ name, room }, callback) => {
         const { error, user } = addUser({ id: socket.id, name, room });
@@ -54,4 +82,44 @@ io.on('connect', (socket) => {
     })
 });
 
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
 server.listen(process.env.PORT || 4000, () => console.log(`Server has started.`));
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'moderator' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
--- BACK/router.js
+++ BACK/router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.send('server is up and running')
+    res.send('server is up and running in XP3000')
 })
 
 module.exports = router
